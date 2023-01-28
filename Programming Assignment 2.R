e7ab578c165fdba2a105d7fea1336687cde13232
rdwyllia <120357855+rdwyllia@users.noreply.github.com>
2023-01-28 15:31:34 -0600

    Add files via upload

---
+++ Programming Assignment 2.R
@@ -0,0 +1,29 @@
+
+### Question 1.)
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix  
+  getInverse <- function() inv  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+
+### Question 2.)
+cacheSolve <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+  message("retrieving cached data")
+  return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv
+}
+
+
